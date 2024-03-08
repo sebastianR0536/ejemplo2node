bda5e0e5ba39384bb91bb896a75f5b5f6c25d1ea
sebastian <juans.rivasquemba@gmail.com>
2024-03-07 20:19:11 -0500

    se agrego nodemon

---
+++ index.js
@@ -0,0 +1,21 @@
+/*'use strict';
+http = require('http');
+
+const server = http.createServer(function(req, res, ){
+res.writeHead(200, { 'content-type':'text/plain'});
+res.end('Hola mundo')
+
+});
+server.listen(5000);*/
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/',(req, res)=>{
+res.send('¡Hola Mundo!,  Sebastian Rivas')
+
+})
+
+app.listen(port, ()=>{
+console.log('la aplicacion se esta ejecutando por el puerto' + port)
+})
